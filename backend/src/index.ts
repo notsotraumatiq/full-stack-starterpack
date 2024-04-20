95df1f6142eb62cbbba1bff567efe7acb89b758d
notsotraumatiq <atiqpatel95@gmail.com>
2024-04-19 21:23:13 -0400

    test done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,80 @@
-import koa from 'koa';
-
+import koa from "koa";
+import Router from "@koa/router";
+import cors from "@koa/cors";
 const app = new koa();
-app.use((ctx) => {
-  ctx.body = 'Hello World';
+const router = new Router();
+const physicians = {
+  1: {
+    id: 1,
+    name: "Dr. John Doe",
+  },
+  2: {
+    id: 2,
+    name: "Dr. Jane Doe",
+  },
+  3: {
+    id: 3,
+    name: "Dr. John Smith",
+  },
+};
+
+const appointments = {
+  1: {
+    id: 1,
+    physicianId: 1,
+    patient: "Alice",
+    time: "2021-01-01T10:00:00Z",
+    kind: "New Patient",
+  },
+  2: {
+    id: 2,
+    physicianId: 1,
+    patient: "Bob",
+    time: "2021-01-01T10:30:00Z",
+    kind: "Follow-up",
+  },
+  3: {
+    id: 3,
+    physicianId: 2,
+    patient: "Charlie",
+    time: "2021-01-01T11:00:00Z",
+    kind: "New Patient",
+  },
+  4: {
+    id: 4,
+    physicianId: 3,
+    patient: "David",
+    time: "2021-01-01T11:30:00Z",
+    kind: "Follow-up",
+  },
+};
+
+// Get list of  physicians on /physicians
+
+app.use(cors({ origin: "*" }));
+app.use((ctx, next) => {
+  if (ctx.path === "/physicians") {
+    ctx.body = Object.values(physicians);
+  } else {
+    return next();
+  }
 });
 
+// Get list of appointments on /appointments:id
+
+router.get("/physicians/:physicianId", (ctx) => {
+  const matchId = Number(ctx.params.physicianId); // Convert matchId to a number
+  if (!Number.isInteger(matchId)) {
+    ctx.status = 400;
+    ctx.body = "Physician ID must be an integer";
+    return;
+  }
+  const result = [];
+  for (let [key, value] of Object.entries(appointments)) {
+    if (value["physicianId"] === matchId) result.push(value);
+  }
+  ctx.body = result;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,116 @@
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+interface Physician {
+  id: number;
+  name: string;
+}
+
+interface Appointment {
+  id: number;
+  physicianId: number;
+  patient: string;
+  time: string;
+  kind: string;
+}
 function App() {
-  return <h1 className="text-center font-bold underline">Hello world!</h1>;
+  const [physicians, setPhysicians] = useState<Physician[]>([]);
+  const [selectedPhysicianId, setSelectedPhysicianId] = useState<number>(0);
+  const [appointments, setAppointments] = useState<Appointment[]>();
+  const fetchPhysiciains = useCallback(async () => {
+    try {
+      const response = await axiosInstance.get("/physicians");
+      setPhysicians(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchPhysiciains();
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="bg-slate-400 h-1/2 shadow-m rounded-m">
+        <div className="flex flex-col my-4 p-4">
+          <h1 className="text-4xl font-bold text-blue-600 ">Test Cal</h1>
+          <h4 className="text-lg font-bold uppercase my-5">Physicians</h4>
+          <ul className="">
+            {physicians.length > 0
+              ? physicians.map((physician) => (
+                  <li
+                    key={physician.id}
+                    className="my-1 cursor-pointer"
+                    onClick={async () => {
+                      try {
+                        const response = await axiosInstance.get(
+                          `/physicians/${physician.id}`
+                        );
+
+                        setAppointments(response.data);
+                      } catch (error) {
+                        console.error(error);
+                      }
+                      setSelectedPhysicianId(physician.id);
+                    }}
+                  >
+                    <h5>{physician.name}</h5>
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
+      </div>
+      {selectedPhysicianId === 0 ? null : (
+        <div className="flex flex-col text-left  h-1/2 px-2">
+          <h4 className="text-5xl mt-10 mb-2 font-bold">
+            {physicians[selectedPhysicianId].name}
+          </h4>
+          <h6 className="text-lg mb-10">
+            {physicians[selectedPhysicianId].name + "email"}
+          </h6>
+
+          <div className="flex-1">
+            {appointments === undefined || appointments.length === 0 ? null : (
+              <table className="border-cyan-950">
+                <thead>
+                  <tr>
+                    <th className="px-2 py-2">#</th>
+                    <th className="px-2 py-2">Patient</th>
+                    <th className="px-2 py-2">Time</th>
+                    <th className="px-2 py-2">Kind</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {appointments.map((appointment) => {
+                    const timeString = new Date(
+                      appointment.time
+                    ).toLocaleTimeString();
+                    return (
+                      <tr key={appointment.id} className="px-2 py-2">
+                        <td className="px-2 py-2">{appointment.id}</td>
+                        <td className="px-2 py-2">{appointment.patient}</td>
+                        <td className="px-2 py-2">{timeString}</td>
+                        <td className="px-2 py-2">{appointment.kind}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
